5584bc6cc95ee6afa58c6e52fe72cf0988f168e2
Steddman Bell <steddfast@gmail.com>
2024-01-30 11:57:38 -0800

    Updating and adding class

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,30 +1,44 @@
 card_number = "5541808923795240"
 
 # Your Luhn Algorithm Here
+class CreditCheck 
+    attr_reader :card_number,
+                :limit,
+                :doubled_num,
+                :results_num,
+                :arrnum3,
+                :is_valid?
 
-# Output
-## If it is valid, print "The number [card number] is valid!"
-## If it is invalid, print "The number [card number] is invalid!"
-def is_valid?
-#convert string to integers in array
-    arrnum1 = card_number.to_i.digits.reverse
+    attr_writer :card_number,
+                :limit
 
-#split into even and odd
-    arrnum2 = arrnum1.select.with_index{|element, index| index.zero? ? index : index % 2 == 0} 
-    arrnum3 = arrnum1.select.with_index{|element, index| (index % 2) != 0}
-
-#double first array elements
-    doubled_num = arrnum2.map{|element| element * 2}
-
-#define sum method to convert to string -> char -> count characters as integers -> sum them together
-    def sum_digits(array)
-        array.to_s.chars.map(&:to_i).sum
+    def initialize(card_number, limit)
+        @card_number = card_number
+        @limit = limit
     end
+    def is_valid?
+        ## If it is valid, print "The number [card number] is valid!"
+        ## If it is invalid, print "The number [card number] is invalid!"
+        #convert string to integers in array
+        arrnum1 = card_number.to_i.digits.reverse
 
-    results_num = sum_digits(doubled_num) + sum_digits(arrnum3)
+        #split into even and odd
+        arrnum2 = arrnum1.select.with_index{|element, index| index.zero? ? index : index % 2 == 0} 
+        @arrnum3 = arrnum1.select.with_index{|element, index| (index % 2) != 0}
 
+        #double first array elements
+        @doubled_num = arrnum2.map{|element| element * 2}
+
+        #define sum method to convert to string -> char -> count characters as integers -> sum them together
+    def sum_digits(array)
+        array.to_s.chars.map(&:to_i).sum 
+    end
+    def results_num()
+        @results_num = sum_digits(@doubled_num) + sum_digits(@arrnum3)
+    end
     if results_num % 10 == 0
         print "The number #{card_number} is valid!"
     else
         print "The number #{card_number} is invalid!"
+    end
 end
